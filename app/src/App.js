66b1ddcecc7dcfe429fb290ea09cca75572eacb3
mtfum <f.yamanaka1127@gmail.com>
2022-04-24 15:54:20 -0700

    Hook up Phantom Wallet

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -7,25 +7,48 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header">🍭 Candy Drop</p>
-          <p className="sub-text">NFT drop machine with fair mint</p>
-        </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
-        </div>
-      </div>
-    </div>
-  );
+	const checkIfWalletIsConnected = async () => {
+		try {
+			const { solana } = window;
+			if (solana && solana.isPhantom) {
+				console.log('Phantom wallet found!');
+				const respoonse = await solana.connect({ onlyIfTrusted: true });
+				console.log('Connected with PublicKey', respoonse.publicKey.toString());
+			} else {
+				alert('Solana onject not foun! Get a Phontom Wallet');
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	useEffect(() => {
+		const onLoad = async () => {
+			await checkIfWalletIsConnected();
+		};
+		window.addEventListener('load', onLoad);
+		return () => window.removeEventListener('load', onLoad);
+	}, []);
+
+	return (
+		<div className="App">
+			<div className="container">
+				<div className="header-container">
+					<p className="header">🍭 Candy Drop</p>
+					<p className="sub-text">NFT drop machine with fair mint</p>
+				</div>
+				<div className="footer-container">
+					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+					<a
+						className="footer-text"
+						href={TWITTER_LINK}
+						target="_blank"
+						rel="noreferrer"
+					>{`built on @${TWITTER_HANDLE}`}</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default App;
